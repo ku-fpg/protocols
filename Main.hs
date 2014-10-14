961fd979e7c4b8391d7ced7b691536ae716afc05
Andy Gill <andygill@ku.edu>
2014-10-13 23:29:02 -0500

    Counting up

--- Main.hs
+++ Main.hs
@@ -1,9 +1,12 @@
+{-# LANGUAGE OverloadedStrings, TupleSections #-}
 module Main where
 
 import Graphics.Blank hiding (Event)
 import FRP.Yampa
 import FRP.Yampa.Vector2
-import Data.Text(Text)
+import Data.Text(Text,pack)
+import Data.Monoid((<>))
+import Control.Arrow
 
 import FRP.Yampa.Canvas
 
@@ -12,6 +15,7 @@ main = blankCanvas 3000 $
        reactimateSFinContext (\ _ -> return NoEvent) id program
 
 program :: SF (Event ()) (Canvas ())
-program = undefined
-
+program = time >>> (arr $ \ t -> do
+ 	font "20pt Comic Sans MS"
+	fillText ("+ " <> pack (show t),50,50))
 
